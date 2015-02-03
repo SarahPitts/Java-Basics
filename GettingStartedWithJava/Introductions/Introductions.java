55c08aa3cbbde1090baa0a5e2a948602388021b6
sarahpitts <mccallsmedia@gmail.com>
2015-02-03 16:48:47 -0600

    initial commit

---
+++ GettingStartedWithJava/Introductions/Introductions.java
@@ -0,0 +1,18 @@
+import java.io.Console;
+ 
+public class Introductions {
+  
+    public static void main(String[] args) {
+        Console console = System.console();
+        // Write things with printf
+        console.printf("Hello");
+  }
+}
+
+//to run: 
+// 1) Save file then Compile
+//  -  javac Introductions.java
+// 2) ls
+// 3) Run
+//  -  java Introductions
+//    - java runs the program with the class name and requires no extension
