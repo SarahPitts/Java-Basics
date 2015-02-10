23ca4c943cf2c9042a6596b31d393a1adccfcfe6
sarahpitts <mccallsmedia@gmail.com>
2015-02-10 14:48:19 -0600

    fleshed out the prototype

--- UsingYourNewTools/TreeStory.java
+++ UsingYourNewTools/TreeStory.java
@@ -10,9 +10,15 @@ public class TreeStory {
             adjective - A description used to modify or describe a noun
             Enter your amazing code here!
         */
-        String name = console.readLine("What is your name?  ");
-        String adjective = console.readLine("How would you describe yourself?  ");
-        console.printf("%s is very %s", name, adjective);
+        String name = console.readLine("Enter a name:  ");
+		String adjective = console.readLine("Enter an adjective:  ");
+		String noun = console.readLine("Enter a noun:  ");
+		String adverb = console.readLine("Enter an adverb:  ");
+		String verb = console.readLine("Enter a verb ending with -ing:  ");
+
+		console.printf("Your Strory:\n.............\n");
+		console.printf("%s is a(n) %s %s.  ", name, adjective, noun);
+		console.printf("They are always %s %s.\n", adverb, verb);
     }
     
 }
