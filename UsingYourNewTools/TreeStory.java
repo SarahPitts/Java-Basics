1c3e1a9e62788851bc80962b40cb91375c694612
sarahpitts <mccallsmedia@gmail.com>
2015-02-17 15:12:01 -0600

    added equals method to string object

--- UsingYourNewTools/TreeStory.java
+++ UsingYourNewTools/TreeStory.java
@@ -30,6 +30,11 @@ public class TreeStory {
         String name = console.readLine("Enter a name:  ");
 		String adjective = console.readLine("Enter an adjective:  ");
 		String noun = console.readLine("Enter a noun:  ");
+            //method for the string object
+            if (noun.equalsIgnoreCase("dork")) {
+                console.printf(" That language is not allowed.  Exiting.  \n\n");
+                System.exit(0);
+            }
 		String adverb = console.readLine("Enter an adverb:  ");
 		String verb = console.readLine("Enter a verb ending with -ing:  ");
 
